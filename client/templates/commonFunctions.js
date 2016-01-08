86e197451958d5f9dca7bcbe1395e1b4e2539a89
troywith77 <1075000368@qq.com>
2016-01-08 16:15:41 +0800

    openbg使用单独的helper

--- client/templates/commonFunctions.js
+++ client/templates/commonFunctions.js
@@ -1,6 +1,15 @@
 Template.registerHelper('showImg', function(url) {
     var frameid = 'frameimg' + Math.ceil(Math.random() * 10 );
     window.iframeId = frameid;
-    window.img = '<img id="img" src=' + url + '?' + Math.random() + ' /><script>window.onload = function() { parent.document.getElementById(""+parent.iframeId+"").height = document.getElementById("img").height+"px"; }<'+'/script>';
-    return ('<iframe id="'+frameid+'" src="javascript:parent.img;" frameBorder="0" scrolling="no" width="100%"></iframe>');
+    window.img = '<img id="img" src=' + url + '?' + Math.random() + 
+    ' style="height:100%" /><script>window.onload = function() { document.getElementsByTagName("body")[0].style.margin="0" }<'+'/script>';
+    return ('<iframe id="'+frameid+'" src="javascript:parent.img;" frameBorder="0" scrolling="no" width="100%" height="100%"></iframe>');
+})
+
+Template.registerHelper('showOpenImg', function(url) {
+    var frameid = 'frameimg' + Math.ceil(Math.random() * 10 );
+    window.iframeId = frameid;
+    window.img = '<img id="img" src=' + url + '?' + Math.random() + 
+    ' style="height:100%;width:100%" /><script>window.onload = function() { document.getElementsByTagName("body")[0].style.margin="0" }<'+'/script>';
+    return ('<iframe id="'+frameid+'" src="javascript:parent.img;" frameBorder="0" scrolling="no" width="100%" height="100%"></iframe>');
 })
--- client/templates/components/openBG/openBg.js
+++ client/templates/components/openBG/openBg.js
@@ -3,11 +3,10 @@ var imgHref = new ReactiveVar('');
 
 Template.openBg.onRendered(function() {
 	Meteor.setTimeout(function() {
-		// time.set('hide');
-	}, 2000)
+		time.set('hide');
+	}, 3000)
 	Meteor.call('getOpenBg', function(error, result) {
 		if(result) {
-			console.log(result);
 			imgHref.set(result.data.img)
 		}
 	})
@@ -18,7 +17,6 @@ Template.openBg.helpers({
 		return time.get()
 	},
 	imgSrc: function() {
-		// return imgHref.get();
-		return UI._globalHelpers.showImg(imgHref.get())
+		if(imgHref.get()) return UI._globalHelpers.showOpenImg(imgHref.get())
 	}
 })
